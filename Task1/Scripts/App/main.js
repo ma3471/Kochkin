fcecb70dcf9505784ecbfeeb4f9ec0e17691eff0
ma3471 <ma3471@mail.ru>
2017-01-29 11:51:29 +0400

    Забыл добавить

---
+++ Task1/Scripts/App/main.js
@@ -0,0 +1,191 @@
+﻿; (function () {
+    var prodStorage = [
+            { id: 1, name: "Товар 1", count: 5, price: 12352.25 },
+            { id: 2, name: "Товар 2", count: 15, price: 12552.25 },
+            { id: 3, name: "Товар 3", count: 150, price: 12452.25 }
+        ],
+        prodShow = [],
+		formatterUsdCur = new Intl.NumberFormat("en-US", {
+		  style: "currency",
+		  currency: "USD"
+		}),
+        currentId = 4,
+        prodIndex = -1,
+        sortingName = false,
+        sortingPrice = false,
+        dbName = document.getElementById("storeName"),
+        dbCount = document.getElementById("storeCount"),
+        dbPrice = document.getElementById("storePrice"),
+        inputPropriety = document.getElementById("storePropriety"),
+        tbodyDb = document.getElementById("tbodyElem");
+
+    function drawDb(prodRow) { // Отрисовывает сведения о товарах.
+        tbodyDb.innerHTML = "";
+        prodRow.forEach(displayBlock);
+    }
+
+	function displayDb() { // Отображение перечня товаров.
+		prodShow = prodStorage;
+		drawDb(prodShow);
+	}
+	
+	function searchDb() { // Фильтрация по имени товара.
+	    var dbFilter = document.getElementById("storeFilter"),
+	        dbFilterUpperCase = dbFilter.value.toUpperCase();
+        prodShow = prodStorage.filter(function (prod) {
+            return prod.name.slice(0, dbFilter.value.length).toUpperCase() === dbFilterUpperCase;
+        });
+        drawDb(prodShow);
+    }
+
+    function sortingNameToggle() { // Меняет направление сортировки по имени.
+        var toggleName = document.getElementById("nameToggle").firstElementChild;
+        if (!prodShow) prodShow = prodStorage;
+		prodShow.sort(function (prodA, prodB) {
+            if (prodA.name.toUpperCase() < prodB.name.toUpperCase()) return -1;
+            return 1;
+        }); 
+        sortingName = !sortingName;
+        if (sortingName) {
+			prodShow.reverse();
+			toggleName.className = "glyphicon glyphicon-triangle-bottom";
+		} else {
+			toggleName.className = "glyphicon glyphicon-triangle-top";
+		}
+        drawDb(prodShow);
+    }
+
+    function sortingPriceToggle() { // Меняет направление сортировки по цене.
+        var togglePrice = document.getElementById("priceToggle").firstElementChild;
+        if (!prodShow) prodShow = prodStorage;
+        prodShow.sort(function (prodA, prodB) {
+            if (prodA.price < prodB.price) return -1;
+            return 1;
+        });
+        sortingPrice = !sortingPrice;
+        if (sortingPrice) {
+			prodShow.reverse();
+			togglePrice.className = "glyphicon glyphicon-triangle-bottom";
+		} else {
+			togglePrice.className = "glyphicon glyphicon-triangle-top";
+		}
+        drawDb(prodShow);
+    }
+
+    function displayBlock(prod) { // Отрисовывает элемент tr - строку перечня.
+        var innerBlock = document.getElementById("rowBlock").innerHTML,
+            trElemBlock = document.createElement("tr");
+        trElemBlock.innerHTML = innerBlock;
+		
+        trElemBlock.getElementsByClassName("prodName")[0].innerHTML = prod.name;
+        trElemBlock.getElementsByClassName("prodCount")[0].innerHTML = prod.count;
+        trElemBlock.getElementsByClassName("prodPrice")[0].innerHTML = formatterUsdCur.format(prod.price);
+        trElemBlock.getElementsByClassName("#prodEditStartModal")[0].setAttribute("data-target", "#editStartModal" + prod.id);
+        trElemBlock.getElementsByClassName("#prodEditStartModal")[0].id = "editAboveModal" + prod.id;
+        trElemBlock.getElementsByClassName("prodEditStartModal")[0].id = "editStartModal" + prod.id;
+        trElemBlock.getElementsByClassName("prodEditNameFor")[0].setAttribute("for", "editName" + prod.id);
+        trElemBlock.getElementsByClassName("prodEditNameId")[0].id = "editName" + prod.id;
+        trElemBlock.getElementsByClassName("prodEditCountFor")[0].setAttribute("for", "editCount" + prod.id);
+        trElemBlock.getElementsByClassName("prodEditCountId")[0].id = "editCount" + prod.id;
+        trElemBlock.getElementsByClassName("prodEditPriceFor")[0].setAttribute("for", "editPrice" + prod.id);
+        trElemBlock.getElementsByClassName("prodEditPriceId")[0].id = "editPrice" + prod.id;
+        trElemBlock.getElementsByClassName("prodEditInModal")[0].id = "editInModal" + prod.id;
+        trElemBlock.getElementsByClassName("#prodDropStartModal")[0].setAttribute("data-target", "#dropStartModal" + prod.id);
+        trElemBlock.getElementsByClassName("#prodDropStartModal")[0].id = "dropAboveModal" + prod.id;
+        trElemBlock.getElementsByClassName("prodDropStartModal")[0].id = "dropStartModal" + prod.id;
+        trElemBlock.getElementsByClassName("prodDropInModal")[0].id = "dropInModal" + prod.id;
+
+        tbodyDb.appendChild(trElemBlock);
+    }
+
+    function getIndex(prodArr, prodId) { // Возвращает индекс товара в массиве по id товара.
+        for (var i = 0; i < prodArr.length; i++) {
+            if (prodArr[i].id === prodId) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function addPlace() { // Размещает добавляемый объект в хранилище объектов.
+        var prodObj = {},
+			correctness = +inputPropriety.value,
+            priceRound;
+
+        if (correctness) {
+            prodObj.id = currentId;
+            prodObj.name = dbName.value;
+            prodObj.count = +dbCount.value;
+            priceRound = +dbPrice.value;
+            prodObj.price = Math.round(priceRound * 100) / 100;
+            prodStorage.push(prodObj);
+            currentId++;
+            searchDb();
+        } 
+    }
+
+    function editRetrieve(prodId) { // Извлекает из хранилища сведения о товаре.
+        prodIndex = getIndex(prodStorage, +prodId);
+        if (prodIndex !== -1) {
+            dbName.value = prodStorage[prodIndex].name;
+            dbCount.value = prodStorage[prodIndex].count;
+            dbPrice.value = prodStorage[prodIndex].price;
+        }
+        document.getElementById("editInModal" + prodId).addEventListener("click", editPlace);
+    }
+
+    function editPlace() { // Размещает отредактированные сведения о товаре в хранилище.
+        setTimeout(function() {
+            var prodObj = {},
+                correctness = +inputPropriety.value,
+                priceRound;
+            if (!!correctness) {
+                prodObj.id = currentId;
+                prodObj.name = dbName.value;
+                prodObj.count = +dbCount.value;
+                priceRound = +dbPrice.value;
+                prodObj.price = Math.round(priceRound * 100) / 100;
+                prodStorage.push(prodObj);
+                currentId++;
+                prodStorage.splice(prodIndex, 1);
+                searchDb();
+            }
+        }, 0);
+        var spareElem = document.querySelector("div.modal-backdrop.fade.in");
+        if (spareElem) { spareElem.parentNode.removeChild(spareElem); }
+    }
+
+    function dropRetrieve(prodId) { // Извлекает удаляемого товара.
+        prodIndex = getIndex(prodStorage, +prodId);
+        document.getElementById("dropInModal" + prodId).addEventListener("click", dropPlace);
+    }
+
+    function dropPlace() { // Удаляет из хранилища объект товара, используя его индекс.
+        prodStorage.splice(prodIndex, 1);
+        searchDb();
+        var spareElem = document.querySelector("div.modal-backdrop.fade.in");
+        if (spareElem) { spareElem.parentNode.removeChild(spareElem); }
+    }
+
+    function tbodyClick(e) { // Кнопки редактирования или удаления.
+        var target = e.target;
+
+        switch (target.id.substring(0, 8)) {
+            case "editAbov":
+                editRetrieve(target.id.substring(14));
+                break;
+            case "dropAbov":
+                dropRetrieve(target.id.substring(14));
+                break;
+            default:
+                break;
+        }
+    }
+
+    window.addEventListener("load", displayDb);
+    document.getElementById("searchAboveModal").addEventListener("click", searchDb);
+    document.getElementById("addInModal").addEventListener("click", addPlace);
+    document.getElementById("nameToggle").addEventListener("click", sortingNameToggle);
+    document.getElementById("priceToggle").addEventListener("click", sortingPriceToggle);
+	tbodyDb.addEventListener("click", tbodyClick);
+})();
---
+++ Task1/Scripts/App/verifyChange.js
@@ -0,0 +1,288 @@
+﻿; (function () {
+    var controlCharMaxNum = 32,
+		firefoxFuncBtnNum = 0,
+        nameMask = /[a-zA-Zа-яА-ЯёЁ0-9 _,;:\\\/\-\.\+\*\(\)!@#\$%&={}\[\]\"\'\?<>№]/,
+        nameWildcard = /^[a-zA-Zа-яА-ЯёЁ0-9 _,;:\\\/\-\.\+\*\(\)!@#\$%&={}\[\]\"\'\?<>№]{1,15}$/,
+        countMask = /[\d]/,
+        priceMask = /[\d\.]/,
+		formatterUsdCur = new Intl.NumberFormat("en-US", {
+		  style: "currency",
+		  currency: "USD"
+		}),
+        flagName = false,
+        flagCount = false,
+        flagPrice = false,
+        inputName,
+        inputCount,
+        inputPrice,
+        changeBtnInModal,
+        changeName = document.getElementById("storeName"),
+        changeCount = document.getElementById("storeCount"),
+        changePrice = document.getElementById("storePrice"),
+        inputPropriety = document.getElementById("storePropriety"),
+        tbodyEdit = document.getElementById("tbodyElem");
+
+    function showError(container, errorMessage) { // Отобразить ошибку.
+        var msgElem = document.createElement("span");
+        msgElem.className = "error-msg";
+        msgElem.innerHTML = errorMessage;
+        container.insertBefore(msgElem, container.firstChild);
+    }
+
+    function resetError(container) { // Сбросить ошибку.
+        if (container.firstChild.className === "error-msg") {
+            container.removeChild(container.firstChild);
+        }
+    }
+
+    function addPrepare() { // Очистка полей input при открытии модального окна.
+        inputName = document.getElementById("addName");
+        inputCount = document.getElementById("addCount");
+        inputPrice = document.getElementById("addPrice");
+        changeBtnInModal = document.getElementById("addInModal");
+        resetError(inputName.parentElement);
+        resetError(inputCount.parentElement);
+        resetError(inputPrice.parentElement);
+        resetError(changeBtnInModal.parentElement);
+        inputPropriety.value = 0;
+        flagName = flagCount = flagPrice = false;
+        inputName.value = inputCount.value = inputPrice.value = "";
+        changeName.value = changeCount.value = changePrice.value = "";
+		inputName.addEventListener("keypress", nameFormat);
+		inputName.addEventListener("paste", nameDeny);
+		inputName.addEventListener("blur", nameCheck);
+		inputCount.addEventListener("keypress", countFormat);
+		inputCount.addEventListener("paste", countDeny);
+		inputCount.addEventListener("blur", countCheck);
+		inputPrice.addEventListener("focus", priceRegain);
+		inputPrice.addEventListener("keypress", priceFormat);
+		inputPrice.addEventListener("paste", priceDeny);
+		inputPrice.addEventListener("blur", priceCheck);
+		changeBtnInModal.addEventListener("click", changeCheck);
+    }
+
+    function editPrepare(prodId) { // Заполнение полей input значениями из хранилища.
+        inputName = document.getElementById("editName" + prodId);
+        inputCount = document.getElementById("editCount" + prodId);
+        inputPrice = document.getElementById("editPrice" + prodId);
+        changeBtnInModal = document.getElementById("editInModal" + prodId);
+        resetError(inputName.parentElement);
+        resetError(inputCount.parentElement);
+        resetError(inputPrice.parentElement);
+        resetError(changeBtnInModal.parentElement);
+        inputPropriety.value = 0;
+        flagName = flagCount = flagPrice = false;
+        inputName.value = changeName.value;
+        inputCount.value = changeCount.value;
+        inputPrice.value = formatterUsdCur.format(+changePrice.value);
+        inputName.addEventListener("keypress", nameFormat);
+        inputName.addEventListener("paste", nameDeny);
+        inputName.addEventListener("blur", nameCheck);
+        inputCount.addEventListener("keypress", countFormat);
+        inputCount.addEventListener("paste", countDeny);
+        inputCount.addEventListener("blur", countCheck);
+        inputPrice.addEventListener("focus", priceRegain);
+        inputPrice.addEventListener("keypress", priceFormat);
+        inputPrice.addEventListener("paste", priceDeny);
+        inputPrice.addEventListener("blur", priceCheck);
+        changeBtnInModal.addEventListener("click", changeCheck);
+    }
+
+    function nameFormat(evt) { // Проверка символов по одному.
+        var e = evt || window.evt,
+            code = e.charCode || e.keyCode,
+            nameSymbol;
+
+        resetError(inputName.parentElement);
+        if (code < controlCharMaxNum ||
+			e.charCode === firefoxFuncBtnNum ||
+			e.ctrlKey || e.altKey) {
+            return;
+        }
+        nameSymbol = String.fromCharCode(code);
+
+        if (!nameMask.test(nameSymbol)) {
+            showError(inputName.parentElement, "* Введите буквы, цифры или спецсимволы");
+        } else if (inputName.value.length >= 15) {
+            showError(inputName.parentElement, "* Имя должно быть от 1 до 15 символов");
+        } else {
+            return;
+        }
+        e.preventDefault();
+    }
+
+    function nameCheck() { // Проверка по завершении редактирования.
+        var nameText = inputName.value.trim(),
+            emptyName = false; 
+        if (nameText === "") {
+            emptyName = true;
+        }
+
+        resetError(inputName.parentElement);
+
+        if (emptyName) {
+            showError(inputName.parentElement, "* Поле не может быть пустым или из одних пробелов");
+        } else if (!nameWildcard.test(nameText)) {
+            showError(inputName.parentElement, "* Должно быть от 1 до 15 букв, цифр или спецсимволов");
+        } else {
+            changeName.value = nameText;
+            inputName.value = nameText;
+            flagName = true;
+            proprietyControl();
+            return;
+        }
+        e.preventDefault();
+    }
+
+    function nameDeny(e) { // Запрещение копирования из буфера обмена.
+        resetError(inputName.parentElement);
+        showError(inputName.parentElement, "* Нельзя копировать из буфера обмена");
+        e.preventDefault();
+    }
+
+    function countFormat(evt) { // Проверка символов на цифры по одному.
+        var e = evt || window.evt,
+            code = e.charCode || e.keyCode,
+            countSymbol;
+
+        resetError(inputCount.parentElement);
+        if (code < controlCharMaxNum ||
+			e.charCode === firefoxFuncBtnNum ||
+			e.ctrlKey || e.altKey) {
+            return;
+        }
+        countSymbol = String.fromCharCode(code);
+
+        if (!countMask.test(countSymbol)) {
+            showError(inputCount.parentElement, "* Нужно вводить цифры");
+        } else if (inputCount.value.length >= 15) {
+            showError(inputCount.parentElement, "* Количество должно быть от 1 до 15 цифр");
+        } else {
+            return;
+        }
+        e.preventDefault();
+    }
+
+    function countCheck() { // Проверка по завершении редактирования.
+        var countNumber = inputCount.value,
+            emptyCount = false;
+        if (countNumber === "") {
+            emptyCount = true;
+        }
+
+        countNumber = +countNumber; // Преобразование к числовому значению.
+        resetError(inputCount.parentElement);
+
+        if (emptyCount) {
+            showError(inputCount.parentElement, "* Введена пустая строка. Повторите ввод");
+        } else if (isNaN(countNumber)) {
+            showError(inputCount.parentElement, "* Введено не число. Повторите ввод");
+        } else if (!countNumber) {
+            inputCount.value = 0;
+            showError(inputCount.parentElement, "* Введён ноль. Повторите ввод");
+        } else {
+            changeCount.value = countNumber;
+            inputCount.value = countNumber;
+            flagCount = true;
+            proprietyControl();
+        }
+    }
+
+    function countDeny(e) { // Запрещение копирования из буфера обмена.
+        resetError(inputCount.parentElement);
+        showError(inputCount.parentElement, "* Нельзя копировать из буфера обмена");
+        e.preventDefault();
+    }
+
+    function priceFormat(evt) { // Проверка символов по одному.
+        var e = evt || window.evt,
+            code = e.charCode || e.keyCode,
+            priceSymbol;
+
+        resetError(inputPrice.parentElement);
+        if (code < controlCharMaxNum ||
+			e.charCode === firefoxFuncBtnNum ||
+			e.ctrlKey || e.altKey) {
+            return;
+        }
+
+        priceSymbol = String.fromCharCode(code);
+
+        if (!priceMask.test(priceSymbol)) {
+            showError(inputPrice.parentElement, "* Введите цифры или десятичную точку");
+        } else if (inputPrice.value.length >= 15) {
+            showError(inputPrice.parentElement, "* Цена должна содержать от 1 до 15 цифр или точку");
+        } else {
+            return;
+        }
+        e.preventDefault();
+    }
+
+    function priceRegain() { // Восстановление отображения цены при получении фокуса.
+        if (changePrice.value) {
+            inputPrice.value = changePrice.value;
+        }
+    }
+
+    function priceCheck() { // Проверка по завершении редактирования.
+        var priceNumber = inputPrice.value,
+            emptyPrice = false;
+
+        if (priceNumber === "") {
+            emptyPrice = true;
+        }
+
+        resetError(inputPrice.parentElement);
+        priceNumber = +priceNumber;
+
+        if (emptyPrice) {
+            showError(inputPrice.parentElement, "* Введена пустая строка. Повторите ввод");
+        } else if (isNaN(priceNumber)) {
+            showError(inputPrice.parentElement, "* Введено не число. Повторите ввод");
+        } else if (!priceNumber) {
+            inputPrice.value = 0;
+            showError(inputPrice.parentElement, "* Введён ноль. Повторите ввод");
+        } else {
+            changePrice.value = Math.round(priceNumber * 100) / 100;
+            inputPrice.value = formatterUsdCur.format(priceNumber);
+            flagPrice = true;
+            proprietyControl();
+        }
+    }
+
+    function priceDeny(e) { // Запрещение копирования из буфера обмена.
+        resetError(inputPrice.parentElement);
+        showError(inputPrice.parentElement, "* Нельзя копировать из буфера обмена");
+        e.preventDefault();
+    }
+
+    function changeCheck(e) { // Проверка корректности ввода.
+        var correctness = +inputPropriety.value;
+        resetError(changeBtnInModal.parentElement);
+        nameCheck();
+        countCheck();
+        priceRegain();
+        priceCheck();
+        if (!correctness) {
+            showError(changeBtnInModal.parentElement, "* Заполните поля правильно");
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    }
+
+    function proprietyControl() { // Проверка корректности ввода.
+        if (flagName && flagCount && flagPrice) {
+            inputPropriety.value = 1;
+        }
+    }
+
+    function tbodyClick(e) { // Обработка нажатия на кнопку редактирования.
+        var target = e.target;
+		if (target.id.substring(0, 8) === "editAbov") {
+			editPrepare(target.id.substring(14));
+		}
+    }
+
+    document.getElementById("addAboveModal").addEventListener("click", addPrepare);
+	tbodyEdit.addEventListener("click", tbodyClick);
+})();
